db5d786725ba8fa3afab805d39740957072dba3b
Sana Najjah <sananajjah4@gmail.com>
2025-07-29 13:19:52 +0200

    revetement

--- src/data/servicesParProjet.js
+++ src/data/servicesParProjet.js
@@ -1,9 +1,9 @@
 const servicesParProjet = {
   maçonnerie: ["Construction neuve","Construction ancienne"],
-  technicoat: [ "Peinture", "Carrelage", "Moquette", "Parquet", "Papier peint", "Autres"],
-  revêtement: ["Pose de dalles de moquette", "Pose parquet", "Retirer un revêtement existant", "Peindre un sol", "Peindre un escalier", "Pose de dalles PVC"],
+  technicoat: ["Construction maison et local", "Construction maison en bois", "Diagnostic Immobilier", "Architecte d'extérieur", "Charpente"],
+  revêtement: ["Peinture", "Carrelage", "Moquette", "Parquet", "Papier peint", "Autres"],
   plomberie: ["Réparation de fuites d'eau", "Changer un robinet", "Déboucher des WC", "Changer une bonde", "Détartrer des toilettes", "Débouchage des canalisations", "Changer un siphon", "Réparer une chasse d'eau"],
-  nettoyage: ["Nettoyage de bureaux", "Nettoyage industriel", "Entretien des espaces verts", "Lavage de vitres", "Désinfection", "Nettoyage après travaux ou rénovation", "Autres"],
+  nettoyage: ["Nettoyage de bureaux", "Nettoyage industriel", "Entretien des espaces verts", "Lavage de vitres", "Désinfection", "Nettoyage après travaux ou rénovation", "autre"],
 };
 
 export default servicesParProjet;
