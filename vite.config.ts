b61f8e87db090f4e298af5c157d505498f9bfde9
Sana Najjah <sananajjah4@gmail.com>
2025-11-13 15:15:11 +0100

    modification css section

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // IMPORTANT pour les chemins relatifs sur IONOS
   plugins: [react()],
-  base: '/',
   build: {
     sourcemap: true,
     outDir: 'dist',
